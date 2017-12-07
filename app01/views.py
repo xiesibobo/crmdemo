8329010cf1a03ff5adf6a195cf0d8c3834403ba4
解思博 <276236023@qq.com>
2017-12-07 16:19:46 +0800

    未使用form验证

--- app01/views.py
+++ app01/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import auth
 from django.db import transaction
 # Create your views here.
-from app01 import models, forms,modelforms
+from app01 import models, forms, modelforms
 
 
 def login(request):
@@ -45,8 +45,37 @@ def questionnaireadd(request):
 
 
 def questionnaireedit(request, clsid, qsnreid):
-    if request.method == "POST":
-        print(request.POST)
+    if request.is_ajax():
+        questions = json.loads(request.POST.get('quetions'))
+        print('初始化数据：',questions, type(questions))
+        for question in questions:
+            print('单个数据：',question)
+            qustion_id = question.get('qustion_id')
+            tp_id = question.get('tp')
+            qtitle = question.get('title')
+            print('开始',qustion_id,tp_id,qtitle)
+            if qustion_id:
+                qobj_set = models.Questions.objects.filter(id=qustion_id)
+                qobj_set.update(title=qtitle, tp=tp_id)
+                qobj=qobj_set.first()
+                if int(tp_id) != 2:
+                    models.Option.objects.filter(qs=qobj).delete()
+            else:
+                qobj=models.Questions.objects.create(title=qtitle, tp=tp_id)
+            if int(tp_id) == 2:
+                for option in question.get('options'):
+                    print('option_id:',option.get('option_id'))
+                    if option.get('option_id') == 'new':
+                        print('开始创建')
+                        if option.get('title') and option.get('score'):
+                            print(qobj)
+                            optobj=models.Option.objects.create(title=option['title'], score=int(option['score']),qs=qobj)
+                            optobj.qs=qobj
+                    else:
+                        optobj = models.Option.objects.filter(id=option.get('option_id'))
+                        if optobj and option.get('title') and option.get('score'):
+                            optobj.update(title=option['title'], score=int(option['score']))
+
         return HttpResponse('Ok')
 
     def get_question():
@@ -55,25 +84,26 @@ def questionnaireedit(request, clsid, qsnreid):
         if not questions_list:
             s['form'] = forms.QuestionsForm()
             yield s
-        for question in questions_list:
-            def get_options():
-                option_list = models.Option.objects.filter(qs=question)
-                for option in option_list:
-                    item = forms.OptionForm({'title': option.title, 'score': option.score})
-                    # print(item)
-                    yield {'item':item,'obj':option}
-            s['form']=forms.QuestionsForm({'title':question.title,'tp':question.tp})
-            s['obj']=question
-            if question.tp == 2:
-                s['options'] = get_options()
-                s['option_class'] = ''
+        else:
+            for question in questions_list:
+                s = {'form': None, 'obj': None, 'option_class': 'hide', 'options': None}
+                def get_options():
+                    option_list = models.Option.objects.filter(qs=question)
+                    for option in option_list:
+                        item = forms.OptionForm({'title': option.title, 'score': option.score})
+                        yield {'item': item, 'obj': option}
 
-            yield s
+                s['form'] = forms.QuestionsForm({'title': question.title, 'tp': question.tp})
+                s['obj'] = question
+                if question.tp == 2:
+                    s['options'] = get_options()
+                    s['option_class'] = ''
+                yield s
 
     # result = {'question_form': get_question()}
     # for i in result['question_form']:
     #     print(i['form'])
-    return render(request, 'questionnaireaedit.html', {'result': get_question(),'cls':clsid,'qsnreid':qsnreid})
+    return render(request, 'questionnaireaedit.html', {'result': get_question(), 'cls': clsid, 'qsnreid': qsnreid})
 
 
 def questionnaireedit2(request, clsid, qsnreid):
@@ -85,14 +115,17 @@ def questionnaireedit2(request, clsid, qsnreid):
 
         else:
             for que in que_list:
-                form=modelforms.QuestionsModelForm(instance=que)
-                temp={'form': form, 'obj': None, 'option_class': 'hide', 'options': None}
-                if que.tp==2:
-                    temp['option_class']=''
+                form = modelforms.QuestionsModelForm(instance=que)
+                temp = {'form': form, 'obj': None, 'option_class': 'hide', 'options': None}
+                if que.tp == 2:
+                    temp['option_class'] = ''
+
                     def inneroption():
-                        option_list=models.Option.objects.filter(qs=que)
+                        option_list = models.Option.objects.filter(qs=que)
                         for option in option_list:
-                            yield {'item':modelforms.OptionModelForm(instance=option), 'obj':option}
-                    temp['options']=inneroption()
+                            yield {'item': modelforms.OptionModelForm(instance=option), 'obj': option}
+
+                    temp['options'] = inneroption()
                 yield temp
-    return render(request,'questionnaireaedit2.html',{'form':inner()})
+
+    return render(request, 'questionnaireaedit2.html', {'form': inner()})
